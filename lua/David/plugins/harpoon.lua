0d987dea8bf9e3609285f49d945f74c338abb5b9
David <davidbosnic99@gmail.com>
2025-07-17 15:48:41 -0600

    cleaned up some stuff keeping things updated

--- lua/David/plugins/harpoon.lua
+++ lua/David/plugins/harpoon.lua
@@ -1,19 +1,36 @@
 return {
-   "ThePrimeagen/harpoon",
-   -- branch = "harpoon2",
-   dependencies = { "nvim-lua/plenary.nvim" },
-   config = function()
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
+	"ThePrimeagen/harpoon",
+	lazy = false,
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-      vim.keymap.set("n", "<leader>a", mark.add_file)
-      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-t>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-n>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-s>", function()
+			harpoon:list():select(4)
+		end)
 
-      vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-      vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-      vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-      vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-      vim.keymap.set("n", "<C-g>", function() ui.nav_file(5) end)
-      vim.keymap.set("n", "<C-c>", function() ui.nav_file(6) end)
-   end
+		vim.keymap.set("n", "<C-g>", function()
+			harpoon:list():select(5)
+		end)
+		vim.keymap.set("n", "<C-c>", function()
+			harpoon:list():select(6)
+		end)
+	end,
 }
--- lua/David/plugins/oil.lua
+++ lua/David/plugins/oil.lua
@@ -1,19 +1,20 @@
 return {
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("oil").setup({
-                columns = { "icon" },
-                keymaps = {
-                    ["<C-h>"] = false,
-                    ["<M-h>"] = "actions.select_split",
-                },
-                view_options = {
-                    show_hidden = true,
-                },
-            })
-            vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
-        end,
-    },
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<M-h>"] = "actions.select_split",
+					["<C-t>"] = false,
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
+			vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
+		end,
+	},
 }
